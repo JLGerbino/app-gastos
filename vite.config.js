9d52121f0733460d0da411901b2b6e77108e5cc4
JLGerbino <joselgerbino@gmail.com>
2025-12-27 08:45:21 -0300

    descargar app manifest

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/app-gastos/",
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.png", "favicon.ico"],
+      manifest: {
+        name: "Vamo&Vamo",
+        short_name: "Vamo",
+        description: "La manera más fácil de compartir gastos",
+        theme_color: "#ADC1c0",
+        background_color: "#ADC1c0",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          }
+        ],
+      },
+    }),
+  ],
+});
+
+
+
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "/app-gastos/",
+//   plugins: [react()],
+// })
 
