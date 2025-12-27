4eeb140e76797f1eae90a273b84d6176e1b0d662
JLGerbino <joselgerbino@gmail.com>
2025-12-27 13:56:01 -0300

    arreglo descargar app manifest

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+
 export default defineConfig({
+  base: "/app-gastos/",
   plugins: [
     react(),
     VitePWA({
@@ -15,23 +17,90 @@ export default defineConfig({
         theme_color: "#ADC1c0",
         background_color: "#ADC1c0",
         display: "standalone",
-        start_url: "/",
+        start_url: "/app-gastos/",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "/app-gastos/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-512x512.png",
+            src: "/app-gastos/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          }
+          },
         ],
       },
     }),
   ],
 });
+
+
+// export default defineConfig({
+//   base: "/gastos-vacaciones/", // 👈 nombre EXACTO del repo
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       registerType: "autoUpdate",
+//       includeAssets: ["favicon.ico"],
+//       manifest: {
+//         name: "Vamo&Vamo",
+//         short_name: "Vamo",
+//         description: "La manera más fácil de compartir gastos",
+//         theme_color: "#ADC1c0",
+//         background_color: "#ADC1c0",
+//         display: "standalone",
+//         start_url: "/gastos-vacaciones/",
+//         scope: "/gastos-vacaciones/",
+//         icons: [
+//           {
+//             src: "/gastos-vacaciones/pwa-192x192.png",
+//             sizes: "192x192",
+//             type: "image/png",
+//           },
+//           {
+//             src: "/gastos-vacaciones/pwa-512x512.png",
+//             sizes: "512x512",
+//             type: "image/png",
+//           },
+//         ],
+//       },
+//     }),
+//   ],
+// });
+
+
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       registerType: "autoUpdate",
+//       includeAssets: ["logo.png", "favicon.ico"],
+//       manifest: {
+//         name: "Vamo&Vamo",
+//         short_name: "Vamo",
+//         description: "La manera más fácil de compartir gastos",
+//         theme_color: "#ADC1c0",
+//         background_color: "#ADC1c0",
+//         display: "standalone",
+//         start_url: "/",
+//         icons: [
+//           {
+//             src: "/pwa-192x192.png",
+//             sizes: "192x192",
+//             type: "image/png",
+//           },
+//           {
+//             src: "/pwa-512x512.png",
+//             sizes: "512x512",
+//             type: "image/png",
+//           }
+//         ],
+//       },
+//     }),
+//   ],
+// });
 
 
 
