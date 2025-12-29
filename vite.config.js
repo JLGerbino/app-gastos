f41ef4505366f87c07336548bcf44bd197351a3b
JLGerbino <joselgerbino@gmail.com>
2025-12-29 14:38:48 -0300

    cambio logos8

--- vite.config.js
+++ vite.config.js
@@ -19,13 +19,15 @@ export default defineConfig({
         start_url: "/app-gastos/",
         icons: [
           {
-            src: "pwa-192x192.png",
+            //src: "pwa-192x192.png",
+            src: "/app-gastos/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any maskable"
           },
           {
-            src: "pwa-512x512.png",
+            // src: "pwa-512x512.png",
+            src: "/app-gastos/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable"
